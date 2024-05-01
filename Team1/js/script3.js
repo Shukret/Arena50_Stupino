0649499bd8c9fc9f07c0f7e2b749f1982e4d06e0
Shukret Piraev <shukret2017@mail.ru>
2024-05-02 00:18:43 +0300

    Add files via upload

---
+++ Team1/js/script3.js
@@ -0,0 +1,26 @@
+function closeAdvertiserInfo(node){
+    node.classList.remove("active");
+}
+
+var advs = document.querySelectorAll(".pbl-adv");
+for (var i = 0;i < advs.length; i++) {
+    advs[i].addEventListener("click",(e)=> {
+        var ads = document.querySelectorAll(".pbl-adv-info");
+        for (var j = 0;j < ads.length; j++) {
+            ads[j].classList.remove("active");
+        }
+        e.target.parentNode.querySelector(".pbl-adv-info").classList.add("active");
+    });
+}
+
+var swiper = new Swiper('.mfl-slider', {
+    centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    }
+});
